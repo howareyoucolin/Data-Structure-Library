1dc4d36c0b4bd5fe5a852e6954ccc46cc46b6ef5
Yuelin Zhao <yuelinzhao@Yuelins-iMac.lan>
2021-02-16 23:27:18 -0500

    add heaps

---
+++ minHeap.js
@@ -0,0 +1,63 @@
+var MinHeap = function(arr=[]){
+    this.arr = arr;
+}
+
+MinHeap.prototype.heapify = function(arr){
+    for(var i = 0; i < arr.length; i++){
+        this.add({val:arr[i]});
+    }
+} 
+
+MinHeap.prototype.size = function(){
+    return this.arr.length;
+} 
+
+MinHeap.prototype.add = function(n){
+    this.arr.push(n);
+    var i = this.arr.length - 1;
+    var j = parseInt( (i - 1)/2 );
+    while(this.arr[i].val < this.arr[j].val){
+        var t = this.arr[i];
+        this.arr[i] = this.arr[j];
+        this.arr[j] = t;
+        i = j;
+        j = parseInt( (i - 1)/2 );
+    }
+}
+
+MinHeap.prototype.peekMin = function(){
+    return this.arr[0];
+}
+
+MinHeap.prototype.popMin = function(){
+    var t = this.arr[0];
+    this.arr[0] = this.arr[this.arr.length - 1];
+    this.arr[this.arr.length - 1] = t;
+    var res = this.arr.pop();
+    var i = 0;
+    var j1 = i*2+1;
+    var j2 = i*2+2;
+    if(j1 >= this.arr.length) return res;
+    if(j2 >= this.arr.length){
+        var j = j1;
+    }
+    else{
+        var j = (this.arr[j1].val < this.arr[j2].val) ? j1 : j2;
+    }
+    while(this.arr[i].val > this.arr[j].val){
+        var t = this.arr[i];
+        this.arr[i] = this.arr[j];
+        this.arr[j] = t;
+        i = j;
+        j1 = i*2+1;
+        j2 = i*2+2;
+        if(j1 >= this.arr.length) return res;
+        if(j2 >= this.arr.length){
+            var j = j1;
+        }
+        else{
+            var j = (this.arr[j1].val < this.arr[j2].val) ? j1 : j2;
+        }
+    }
+    return res;
+}
